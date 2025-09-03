1575d62c1536f5d180c46566a5b337d6c1c4c2b9
Stes-Galya <stecgala73@gmail.com>
2025-09-03 19:34:47 +0300

    2

--- js/test.js
+++ js/test.js
@@ -1,8 +1,40 @@
-const name = "Galua";
-console.log(name);
-let lastName = "Stes";
-console.log(lastName);
+// const name = "Galua";
+// console.log(name);
+// let lastName = "Stes";
+// console.log(lastName);
 // const namber2 = 5>2;
 // console.log(namber2);
+// let namber = 10;
+// namber = namber + 5
+// namber += 7;
+// console.log(namber);
+// const message = "у мене дуже поганий інтернет, ледве вас чую fghjkuytrV NHJIUOL:PPKJG sd";
+// console.log(message.length);
+// console.log(typeof(message));
+// const upperMessage = message.toUpperCase()
+// console.log(upperMessage);
+//   модалка  alert("Galua")
+// const namber3 = "porpul"
+// alert(namber3)
+// const message5 = "у мене дуже поганий інтернет, ледве вас чую fghjkuytrV NHJIUOL:PPKJG sd";
+// const result = confirm(message5)
+// console.log(result);
+// const userName = prompt("your old")
+// console.log(userName);
+//  console.log(typeof(userName));
+// if (userName === "10") {
+//     console.log("yra");
+// }
+   // Створіть дві змінні, в яких буде зберігатись ваш вік і вік вашого друга. Сумму двох змінних виведіть на екран за допомогою console.log().
+const  meAge = 15
+console.log(meAge);
+
+ const frendAge = 18
+console.log(frendAge);
+const allAge = meAge + frendAge
+console.log(allAge);
 
 
+const cantry = prompt("cantry")
+cantry city = prompt("city")
+console.log("Вы живете в",city," вы знаходетесь "б cantry);
