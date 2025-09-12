dad9d467198c18ecee070e9e357fd5798fedebfe
Stes-Galya <stecgala73@gmail.com>
2025-09-12 19:30:47 +0300

    string

---
+++ js/string.js
@@ -0,0 +1,41 @@
+// console.dir(String)
+// const name = "Galua hgjoek devryu frtyyn   "
+// console.log(name[name.length-1]);
+
+// console.log(name.length);
+// const userName = "Galua";
+// const lastName = "Stes";
+// const fulName = userName +" " + lastName;
+// console.log(fulName);
+
+// const fulName2 = `Привіт мене звати ${userName},Моє призвище ${lastName}`
+// console.log(fulName2);
+
+// const message = "GGhjiejuuIHIOJujdidi";
+// const loverMassge = message.toLowerCase();
+// console.log(loverMassge);
+
+// const message2 = "hjdinKLLOIUBNKkdcnkidninn";
+// const upperMassge = message.toUpperCase();
+// console.log(upperMassge);
+
+// const userLogin = "galua";
+// const inputLogin = prompt("Ведіть логін").toLowerCase().trim();
+// console.log( userLogin === inputLogin);
+
+// const rezalt = "fggiolllkjhgvcxdfyu"
+// console.log(rezalt[0]);
+
+// console.log(rezalt.charAt(rezalt.length-1));
+
+
+
+
+
+const message = "Lorem  sit ipsum dolor   sit amet consectetur adipisicing elit. Sit temporibus culpa sequi necessitatibus. Aliquid temporibus eaque optio ipsam mollitia repellendus similique officiis, cumque ut modi, quaerat magni, sint dolores voluptate?"
+// console.log(message.indexOf("sit"));
+// console.log(message.includes("Galua"));
+console.log(message.startsWith("Lorem"));
+console.log(message.endsWith("?"));
+
+
