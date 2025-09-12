4dfd28af74248d53a905237a41b05cdacd794f7b
Stes-Galya <stecgala73@gmail.com>
2025-09-12 18:09:04 +0300

    3

---
+++ js/mase.js
@@ -0,0 +1,30 @@
+// console.dir(Math);
+// const max = Math.max(2);
+// console.log(max);
+const pov = Math.pow(5,5)
+console.log(pov);
+
+const all = Math.floor(12.9)
+console.log(all);
+
+const sail = Math.ceil(14.1);
+console.log(sail);
+
+const round = Math.round(12.6)
+console.log(round);
+
+const sprt = Math.sqrt
+console.log(sprt);
+
+const random = Math.random( ) * (100 - 10) + 10;
+console.log(random);
+
+// const hait = "23cm";
+// const width = "10cm";
+// const mass1 = parseInt "hait";
+// const mass2 = parseInt "width";
+// const  mass = mass1 + mass2;
+// console.log(mass);
+
+
+
