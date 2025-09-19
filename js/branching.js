174754cb79b7a07ecc0f9470f0c2e921f22eee42
Stes-Galya <stecgala73@gmail.com>
2025-09-19 19:24:04 +0300

    1

---
+++ js/branching.js
@@ -0,0 +1,49 @@
+// 6 значень false
+// console.log(Boolean(""));
+// console.log(Boolean(false));
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean(null));
+// let namber ;
+// console.log(Boolean(namber));
+
+// логычний оператор і "&&"
+// повертає : якщо всі значення призводяться до тру повертає останне знасення якщо є false його й повертвє
+// console.log(2 && 1 &&  0 && "40");
+// const imput = prompt("yure old");
+// console.log(imput);
+
+// const name = "Galya";
+// const rezalt = name && imput && "перевірку пройшли";
+// console.log(rezalt);
+
+// Логічний  аператор або (||) повертає перше Tru яке зустрів якщо всі false то останншй false
+// console.log( 0 || undefined);
+
+
+
+// const name = prompt ("naame");
+// const age = prompt ("age");
+// console.log(name || age);
+// console.log(0 || "age" && 0);
+
+
+
+
+
+//  Логічний  аператор  не-(!) перетворюэ булове значення на протележне
+
+
+
+console.log(!Boolean(true));
+console.log(99 && 21 || 125 && !false);
+
+
+
+
+
+
+
+
+
