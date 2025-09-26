de5d9672e3cf1c75cb6798c5c98c3c210b64149f
Stes-Galya <stecgala73@gmail.com>
2025-09-26 19:30:54 +0300

    svitch

---
+++ js/svitch.js
@@ -0,0 +1,84 @@
+// let name = "Galya"
+// if (10>5) {
+//     let name = "Lev"
+//     console.log(name);
+    
+// }
+//  console.log(name);
+
+
+// if (10>5) {
+//     let name = "Lev"
+
+    
+// }
+//  console.log(name);
+
+// let name = "Galya"
+// if (10>5) {
+    
+//     console.log(name);
+    
+// }
+// let name = "Galya"
+// if (10>5) {
+//     name ="Nazar "
+//     console.log(name);
+    
+// }
+// let name = "Galya"
+// if (10>5) {
+//     let name = "Karina"
+//     name ="Nazar "
+//     console.log(name);
+    
+// }
+//   console.log(name);
+// let name = "Galya"
+// if (10>5) {
+//     let name = "Karina"
+//     name ="Nazar "
+//     console.log(name);
+//     if (10>3){
+// name = "Alina"
+//   console.log(name);
+//     }
+// }
+// ALINA^
+// let name = "Galya"
+// if (10>5) {
+//  name = "Karina"
+//     name ="Nazar "
+
+//     if (10>3){
+// name = "Alina"
+
+//     }
+
+// }
+//   console.log(name);
+
+// let name = "Galya"
+// if (10>5) {
+//  name = "Karina"
+//     name ="Nazar"
+//   console.log(name);
+//     if (10>3){
+//         let name = "B"
+// name = "Alina"
+//   console.log(name);
+//     }
+
+// }
+//   console.log(name);
+let count = 0;
+count = 10;
+if (true){
+ count = 1;
+    count = 20;
+console.log(count);
+
+}
+
+console.log(count);
+
